5b1b9aa8ec9cae5c8daaeff2b27aa7d2b965d019
michel-ferreira <michel_jaf580@hotmail.com>
2021-10-29 15:17:14 +0100

    Add project files.

---
+++ WeatherApp.Web/Data/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Web.Data.Entities
+{
+    public class User
+    {
+    }
+}
